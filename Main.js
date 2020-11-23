c632f553eff0b11a31df7ba93fbefc0ff8c1b8eb
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2020-11-23 17:55:39 +0100

    added home page with button; still have to finish the whole logic with boundaries, score, lives, and game over

--- Main.js
+++ Main.js
@@ -1,5 +1,19 @@
 let fallingWords = document.getElementById('fallingWords')
 let wordInput = document.getElementById('word-input')
+let fallingSingleWord = document.getElementsByClassName("falling")
+let buttonStart = document.querySelector("button")
+
+let homePage = document.querySelector('.home')
+let gamePage = document.querySelector('#game')
+
+
+function showGame () {
+  homePage.style.visibility = "hidden";
+  gamePage.style.visibility = "visible";
+}
+
+
+
 
 const wordsList = [
   'hat',
@@ -26,34 +40,52 @@ const wordsList = [
   'magic',
   'master',
   'space',
-  'definition'
+  'definition',
+  'roller coaster',
+  'IronHack'
 ];
 
-const animClasses = [
-  'falling-middle',
-  'falling-far-left',
-  'falling-far-right'
+const colors = [
+  'blue',
+  'red',
+  'green'
 ];
 
 function generateWord () {
+  const randomPx = Math.floor(Math.random() * 700) + 10
+  const randomSize = Math.floor(Math.random() * 4) + 1  
   const randomElement = wordsList[Math.floor(Math.random() * wordsList.length)];
-  const randomAnimation = animClasses [Math.floor(Math.random() * animClasses.length)];
-  fallingWords.innerHTML = `<h2 class="falling-middle">${randomElement}</h2>`
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  fallingWords.innerHTML += `<h2 class="falling" style="left: ${randomPx}px; font-size: ${randomSize}em; color: ${randomColor};">${randomElement}</h2>`
 }
 
 
+
 function matchWords() {
-  if (wordInput.value === fallingWords.querySelector(".falling-middle").textContent) {
+  
+  if (wordInput.value === fallingWords.querySelector(".falling").textContent) {
     console.log('Correct!!!');
+    
+    fallingWords.removeChild(fallingWords.firstElementChild)
     generateWord()
     wordInput.value = ""
   } 
   
+
   else {
+
     wordInput.value = ""
     console.log("Wrong")
   }
 }
+
+// function checkWordPos () {
+//   setInterval(() => {
+//     let word = document.querySelector(".falling")
+//     if (word)
+//   }, 30);
+// }
+
 
 
 wordInput.addEventListener('keypress', function (e) {
@@ -62,24 +94,18 @@ wordInput.addEventListener('keypress', function (e) {
   }
 });
 
-
-generateWord()
-
-
-// function removeWord () {
-//   fallingWords.textContent= ""
-//   inputWord.value = ""
-// }
-
-
- 
+buttonStart.addEventListener('click', () => {
+  showGame();
+  generateWord()
+})
 
 
 
 
-
-
-
+function removeWord () {
+  fallingWords.textContent= ""
+  inputWord.value = ""
+}
 
 // function addWordToHtml () {
 //   generateWord();
